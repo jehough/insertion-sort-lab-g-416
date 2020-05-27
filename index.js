9727f490b7b05e12ceb1f4183a05aff13d11da26
Jim Hough <teachlife@outlook.com>
2020-05-27 11:30:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function findMinAndRemove(array){
-
+  let min = arr[0]
+  let minIndex = 0
+  for(let i= 0; i < array.length; i++ ){
+    if (arr[i] < min){
+      min = arr[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
